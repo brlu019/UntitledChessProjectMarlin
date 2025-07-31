1eea0db7c93d8870922d42f6571c22cf3f336a7e
Brian Lu <brlu019@berkeley.edu>
2025-07-31 14:12:53 -0700

    Commit for repo

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3504,7 +3504,7 @@
    * Comment *_STALL_SENSITIVITY to disable sensorless homing for that axis.
    * @section tmc/stallguard
    */
-  #define SENSORLESS_HOMING // StallGuard capable drivers onlyY
+  #define SENSORLESS_HOMING // StallGuard capable drivers only
 
   #if ANY(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
